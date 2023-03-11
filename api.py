71534d63dcc5de7bb26c58cd629a127a447a493b
Harrison Grias <harrison.grias@gmail.com>
2023-03-11 16:54:09 -0600

    feat: add process_receipts endpoint
    chore: remove comments

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import uuid
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+receipts = {}
+
+
+@app.route("/receipts/process", methods=["POST"])
+def process_receipts():
+    if request.method == "POST":
+        data = request.get_json()
+        receipt_id = str(uuid.uuid4())
+        receipts[receipt_id] = data
+        return jsonify({"id": receipt_id})
